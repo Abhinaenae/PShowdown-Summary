76cb722847b81699ac20b8f7a71ded34e013daaf
Abhinay Lavu <92381984+Abhinaenae@users.noreply.github.com>
2023-11-15 17:13:10 -0600

    Add files via upload

---
+++ project/variables.tf
@@ -0,0 +1,14 @@
+variable "acc_key" {
+  description = "Access Key"
+  #default = ""
+  type = string
+}
+variable "sec_key" {
+  description = "Secret Key"
+  #default = ""
+  type = string
+}
+
+variable "bucketname" {
+  default = "abhiwebproject"
+}
